ce8b762baedfde18e32c99ee3e0133e17c8c9c76
Omar Ilpa <omarilpa09@gmail.com>
2025-11-25 15:47:16 +0200

    returned the Index.tsx file

---
+++ src/pages/Index.tsx
@@ -0,0 +1,33 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import LogoSlider from "@/components/LogoSlider";
+import Services from "@/components/Services";
+import Projects from "@/components/Projects";
+import About from "@/components/About";
+import TeamworkQuote from "@/components/TeamworkQuote";
+import ConsultationForm from "@/components/ConsultationForm";
+import Testimonials from "@/components/Testimonials";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import BrochurePopup from "@/components/BrochurePopup";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      {/* <LogoSlider /> */}
+      <Services />
+      <Projects />
+      <About />
+      {/* <TeamworkQuote /> */}
+      <ConsultationForm />
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
+      {/* <BrochurePopup /> */}
+    </div>
+  );
+};
+
+export default Index;
